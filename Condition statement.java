8fe6d16f414ba44ee97a395d6f22a94915c82dcf
Sonal Kumar <75514302+sonal290898@users.noreply.github.com>
2022-07-06 14:55:19 +0530

    Create Condition statement.java

---
+++ Condition statement.java
@@ -0,0 +1,14 @@
+public class Salary{
+    public static void main(String args []){
+        int salary=2000;
+        if(salary > 300){
+            salary = salary+645;
+        }
+        
+            else{
+                salary= salary-54;
+            }
+                
+        System.out.println(salary);
+    }
+}
